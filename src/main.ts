fb5a675e4fb6edaadc19ef53589f95c2a4a0206b
voskanvl <voskanvl@gmail.com>
2023-03-14 15:57:49 +0300

    burger + menu modal

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,29 @@
 import "./sass/style.sass";
 // import '@splidejs/splide/css';
+
+//---toggle menu modal ---
+
+const menuModal = document.querySelector<HTMLElement>(".modal-menu");
+if (!menuModal) throw Error("there isn't .modal-menu");
+
+const burger = document.querySelector<HTMLElement>(".header__burger");
+!!burger &&
+    burger.addEventListener("click", () => {
+        const isClosed = menuModal.classList.contains("closed");
+        if (isClosed) {
+            menuModal.classList.remove("closed");
+            document.body.style.height = "100vh";
+            document.body.style.overflow = "hidden";
+        }
+    });
+
+const closeElement = document.querySelector<HTMLElement>(".modal-menu__close");
+!!closeElement &&
+    closeElement.addEventListener("click", () => {
+        const isOpened = !menuModal.classList.contains("closed");
+        if (isOpened) {
+            menuModal.classList.add("closed");
+            document.body.style.height = "";
+            document.body.style.overflow = "";
+        }
+    });
